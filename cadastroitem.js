828e6a5e990195cd57b261f561a21e5ef3cb265e
leticia sobrinho <leticia.sobrinho@portalsesi.org.br>
2023-05-26 17:16:43 -0300

    logica2605

---
+++ cadastroitem.js
@@ -0,0 +1,116 @@
+const nome = document.getElementById("nome");
+const descricao = document.getElementById("descricao");
+const botaocadastrar = document.querySelector("#btncadastrar");
+
+femaillogado();
+
+var url = new URL (window.location.href);
+var peditar = url.searchParams.get ("peditar");
+var pindice = url.searchParams.get("indice");
+
+if (peditar == "true"){
+    editar (pindice);
+}
+
+
+
+botaocadastrar.onclick = (evento) =>{
+    if ((peditar != "true")|| (peditar == null)){
+    evento.preventDefault();
+    fenvio()
+    .then(result =>{
+        if(result){
+            let dados = JSON.parse(localStorage.getItem("catalogo")) || [];
+            dados.push(
+        {
+            nome : nome.value,
+            descricao : descricao.value,
+            foto  : nomeArq,        
+        }
+    )
+
+    localStorage.setItem("catalogo", JSON.stringify(dados));
+    window.location.assign("catalago.html");
+        }
+
+        else{
+            alert("Houve erro no envio do arquivo");
+        }
+    });
+} 
+else{
+    editarenvio (evento);
+    window.location.assign("catalagoitem.html");
+}
+
+} 
+
+
+
+var nomeArq;
+async function fenvio() { 
+    const url = 'http://localhost:3005/upload';
+    const arquivo = document.getElementById("foto").files[0];
+    const formData = new FormData();
+    formData.append('arquivo', arquivo);
+    console.log(JSON.stringify(formData.values[0]));
+    console.log(JSON.stringify(formData.values[1]));
+    try{
+         
+         var resp = await fetch(url, {
+                                       method: 'POST',
+                                       body: formData,
+                                     }
+                               ) 
+         console.log(resp);
+         if (resp.ok){
+           let respText = await resp.text();
+           nomeArq = respText;
+           return true;
+         }
+         else{
+              return false;
+         }
+       }
+    catch (error) {
+        console.error(error);
+        return false;
+      }
+}
+
+function editar (indice){
+    nome.value = "";
+    descricao.valjue = "";
+    foto.files[0] = null;
+    let dados = JSON.parse(localStorage.getItem
+        ("catalago"));
+    nome.value = dados[indice].nome;
+    descricao.value = dados [indice].descricao;
+    fotoa = dados [indice].foto;
+}
+
+var fotoa;
+function editarenvio(evento){
+    evento.preventDefault();
+    if ((fotoa != foto.value) && (foto.value != "")){
+
+        fenvio()
+        .then(result =>{
+            if(result){
+                salvaEdicao(nomeArq);
+            }
+        })
+    }
+    else{
+        salvaEdicao(fotoa);
+    }
+}
+
+function salvaEdicao (pfoto){
+    let dados = JSON.parse(localStorage.getItem("catalago"));
+    dados[pindice].nome = nome.value;
+    dados[pindice].dscricao = descricao.value;
+    dados[pindice].foto = pfoto;
+    dados[pindice].email = emaillogado;
+    localStorage.setItem("catalago",JSON.stringify(dados));
+}
